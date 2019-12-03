a6c89fdbe8bf8617a5584fb02cd82b26dacbc11a
Vivek Patel <vivek.a.patel@capgemini.com>
2019-12-03 14:23:10 +0530

    added url to product-list

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -2,19 +2,22 @@
 export const productlist = [
     {
         name: 'Iron Man',
-        url: 'https://www.google.com/',
+        url: 'https://en.wikipedia.org/wiki/Iron_Man',
+        description: 'Iron Man is a fictional superhero by Marvel Comics'
+    },
+    {
+        name: 'Iron Man 2008',
+        url: 'https://en.wikipedia.org/wiki/Iron_Man_(2008_film)',
         description: 'Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics'
     },
     {
-        name: 'Iron Man 2',
+        name: 'Iron Man 2010',
+        url: 'https://en.wikipedia.org/wiki/Iron_Man_2',
         description: 'Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics'
     },
     {
-        name: 'Iron Man 3',
+        name: 'Iron Man 2013',
+        url: 'https://en.wikipedia.org/wiki/Iron_Man_3',
         description: 'Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics'
-    },
-    {
-        name: 'Iron Man 4',
-        description: 'Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics'
-    },
+    }
 ]
